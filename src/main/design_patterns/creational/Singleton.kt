c0585147a0d13d7d367c1431312ea0ca72bf4e2e
Philipp Becker <philipp.becker@wow-group.com>
2025-12-12 08:58:19 +0100

    add more singleton options

--- src/main/design_patterns/creational/Singleton.kt
+++ src/main/design_patterns/creational/Singleton.kt
@@ -1,5 +1,5 @@
 // Example of Singleton Design Pattern in Kotlin
-// Empfohlen: Benutzung von object Deklaration
+// Option1 - Empfohlen: Benutzung von object Deklaration
 object Database {
     private val connectionPool = mutableListOf<String>()
 
@@ -12,10 +12,40 @@ object Database {
     fun getConnections(): List<String> = connectionPool
 }
 
+// Option 2 - Object Declaration mit lazy Initialisierung
+object MyDatabaseOptionTwo {
+    // Singleton properties and methods go here
+    fun doSomething() {
+        println("Singleton is doing something")
+    }
+}
+
+// Option 3 - Klassische Implementierung mit Companion Object
+class MyDatabaseOptionThree private constructor() {
+
+    companion object {
+        private val instance: MyDatabaseOptionThree by lazy { MyDatabaseOptionThree() }
+
+        fun getInstance(): MyDatabaseOptionThree {
+            return instance
+        }
+    }
+    // Singleton properties and methods go here
+    fun doSomething() {
+        println("Singleton is doing something")
+    }
+}
+
 // Nutzung des Singleton
 fun main() {
+    // Option 1
     val db1 = Database.connect()
     val db2 = Database.connect()
+    println("Established Connections: ${Database.getConnections()}") // Established Connections: [Connection-1, Connection-2]}
 
-    println("Established Connections: ${Database.getConnections()}") // Established Connections: [Connection-1, Connection-2]
-}
+    // Option 2
+    MyDatabaseOptionTwo.doSomething()
+
+    // Option 3 - Klassische Implementierung mit Companion Object
+    val singletonInstance = MyDatabaseOptionThree.getInstance()
+    singletonInstance.doSomething()
