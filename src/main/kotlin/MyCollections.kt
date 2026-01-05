e65afcae60b1ff7588fcf677d053d5343cf7399d
Philipp Becker <philipp.becker@wow-group.com>
2026-01-05 11:16:43 +0100

    add special functions

--- src/main/kotlin/MyCollections.kt
+++ src/main/kotlin/MyCollections.kt
@@ -77,8 +77,10 @@ fun main(){
     // ++++++++++++++++++++++++++++++++++++++++++++++
     // sequences
     // ++++++++++++++++++++++++++++++++++++++++++++++
-    // sind "lazy" und erzeugen nicht neue Instanzen, wenn man z.B. Listen verarbeitet
-    // und daraus neue Listen entstehen
+    // sind "lazy" und erzeugen keine neue Instanzen. Berechnungen werden nur ausgeführt, wenn sie benötigt werden.
+    // Im Gegensatz zu Listen:
+    // Wenn Listen bearbeitet werden, werden die Berechnungen sofort ausgeführt (eager) und es entsteht eine neue Liste.
+    // Verwende sequences bei große Datenmengen
     val mySequence = sequenceOf("One", 2, true, "abc", 33)
 
     val first = mySequence
@@ -86,10 +88,30 @@ fun main(){
         .map(Any::toString) // transformation
         .first{ it.startsWith("a")}
     println(first)
-
 }
 
 fun showStandardFunctions(){
+
+    // Übersicht:
+    // 1. filter(): Filtert Elemente nach einer Bedingung.
+    // 2. map(): Transformiert jedes Element in etwas anderes z.B. Strings in int count
+    // 3. flatMap(): mappt jedes Element zu einer Collection und „flacht“ das Ergebnis.
+    // 4. groupBy(): Gruppiert Elemente nach einem Schlüssel.
+    // 5. partition(): Teilt eine Collection in zwei: eine mit Elementen, die die Bedingung erfüllen, eine mit denen, die es nicht tun.
+    // 6. associateBy(): Erstellt eine Map, wobei der Schlüssel aus jedem Element berechnet wird.
+    // 7. any, all, none: Prüfen Bedingungen auf Elementen.
+    // 8. reduce und fold: Fassen eine Collection zu einem einzigen Wert zusammen.
+    // 9. zip(): Kombiniert zwei Listen Element für Element zu Paaren.
+    // 10. chunked(): Teilt eine Liste in kleinere Listen einer bestimmten Größe.
+    // 11. forEach(): Führt eine Aktion für jedes Element der Sammlung aus.
+    // 12. first() / last(): Gibt das erste bzw. das letzte Element einer Sammlung zurück.
+    // 13. sorted(): Gibt eine neue Sammlung zurück, die die Elemente in aufsteigender Reihenfolge enthält.
+    // 14. take(): Gibt die ersten n Elemente einer Sammlung zurück.
+    // 15. drop(): Entfernt die ersten n Elemente einer Sammlung und gibt die restlichen zurück.
+    // 16. distinct(): Gibt eine Sammlung zurück, die nur die einzigartigen Elemente enthält, ohne Duplikate.
+    // 17. count(): Zählt die Anzahl der Elemente, die einer Bedingung entsprechen.
+    // 18. find(): Gibt das erste Element zurück, das eine gegebene Bedingung erfüllt, oder null, wenn kein solches Element gefunden wird.
+
     // 1. Filter
     // Filtert Elemente nach einer Bedingung.
     val numbers = listOf(1, 2, 3, 4, 5, 6)
@@ -157,6 +179,54 @@ fun showStandardFunctions(){
     val numbers5 = (1..10).toList()
     val chunks = numbers5.chunked(3)
     println(chunks) // [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]
+
+    // 11. forEach
+    // Führt eine Aktion für jedes Element der Sammlung aus.
+    val numbers6 = (1..10).toList()
+    numbers6.forEach { println(it) }
+
+    // 12. First / Last
+    // Gibt das erste bzw. das letzte Element einer Sammlung zurück.
+    val first = numbers6.first()  // Ausgabe: 1
+    val last = numbers6.last()    // Ausgabe: 10
+
+    // 13. sorted
+    // Gibt eine neue Sammlung zurück, die die Elemente in aufsteigender Reihenfolge enthält.
+    val numbers7 = listOf(3, 1, 2, 4, 5)
+    val sorted = numbers7.sorted()
+    val sortedDes = numbers7.sortedDescending()
+    println(sorted) // Ausgabe: 1 2 3 4 5
+    println(sortedDes) // Ausgabe 5 4 3 2 1
+
+    // 14. take()
+    // Gibt die ersten n Elemente einer Sammlung zurück.
+    val numbers8 = listOf(1, 2, 3, 4, 5)
+    val firstTwo = numbers8.take(2)
+    println(firstTwo)  // Ausgabe: [1, 2]
+
+    // 15. drop()
+    // Entfernt die ersten n Elemente einer Sammlung und gibt die restlichen zurück.
+    val numbers9 = listOf(1, 2, 3, 4, 5)
+    val dropFirstTwo = numbers9.drop(2)
+    println(dropFirstTwo)  // Ausgabe: [3, 4, 5]
+
+    // 16. distinct()
+    // Gibt eine Sammlung zurück, die nur die einzigartigen Elemente enthält, ohne Duplikate.
+    val numbers10 = listOf(1, 2, 2, 3, 3)
+    val distinctNumbers = numbers10.distinct()
+    println(distinctNumbers)  // Ausgabe: [1, 2, 3]
+
+    // 17. count()
+    // Zählt die Anzahl der Elemente, die einer Bedingung entsprechen.
+    val numbers11 = listOf(1, 2, 3, 4, 5)
+    val countEven = numbers11.count { it % 2 == 0 }
+    println(countEven)  // Ausgabe: 2
+
+    // 18. find()
+    // Gibt das erste Element zurück, das eine gegebene Bedingung erfüllt, oder null, wenn kein solches Element gefunden wird.
+    val numbers12 = listOf(1, 2, 3)
+    val firstEven = numbers12.find { it % 2 == 0 }
+    println(firstEven)  // Ausgabe: 2
 }
 
 data class PersonPart(val id: Int, val name: String)
